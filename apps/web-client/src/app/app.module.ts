d0ef2adccbf21b267b0f6720f674e2fe34d6622a
juanmesa2097 <juanmesa2097@gmail.com>
2021-10-03 03:18:13 -0500

    chore(workspace): bump workspace version

--- apps/web-client/src/app/app.module.ts
+++ apps/web-client/src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { HttpClientModule } from '@angular/common/http'
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { AppComponent } from './app.component'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
